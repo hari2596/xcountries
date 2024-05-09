db086e0ef03f57de262a5b057f214eb2f5c8300b
hari2596 <>
2024-05-09 13:50:11 +0530

    css seperated

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,65 +1,35 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import "./Countries.css";
 
-const Tile= ({flagUrl, name ,altFlag})=>{
+const Tile = ({ flagUrl, name, altFlag }) => {
     return (
-       
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "10px",
-            padding: "10px",
-            border: "1px solid black",
-            borderRadius: "8px",
-            flexDirection: "column",
-            width: "200px",
-          }}
-        >
-          <img
-            src={flagUrl}
-            alt={altFlag}
-            style={{ width: "100px", height: "100px" }}
-          />
-          <h2>{name}</h2>
+        <div className="tile">
+            <img src={flagUrl} alt={altFlag} className="flag-image" />
+            <h2>{name}</h2>
         </div>
-      
-    )
-}
+    );
+};
 
 function Countries() {
-  const API_URL = "https://restcountries.com/v3.1/all";
-  const [countries, setCountries] = useState([]);
+    const API_URL = "https://restcountries.com/v3.1/all";
+    const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => setCountries(data));
-  }, []);
+    useEffect(() => {
+        fetch(API_URL)
+            .then((response) => response.json())
+            .then((data) => setCountries(data));
+    }, []);
 
-  console.log({ countries });
+    console.log({ countries });
 
-  return (
-    <div
-    style={{
-        
-      display: "flex",
-     
-      justifyContent: "center",
-      alignContent: "center",
-      height: "100vh",
-      flexWrap: "wrap",
-      marginTop: "20px", // Adjusted marginTop
-      paddingTop: "20px", // Added paddingTop
-    }}
-  >
-   {countries.map((country) => (
-    <Tile flagUrl={country.flags.png} name={country.name.common} altFlag={country.flags.alt}  />
-))}
- 
-    </div>
-  );
+    return (
+        <div className="countries">
+            {countries.map((country) => (
+                <Tile key={country.cca2} flagUrl={country.flags.png} name={country.name.common} altFlag={country.flags.alt} />
+            ))}
+        </div>
+    );
 }
 
 export default Countries;
